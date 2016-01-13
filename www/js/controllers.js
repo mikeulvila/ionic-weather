e94aef3f21de1b64829b27e55cae75a6e567edbe
Mike Ulvila <mikeulvila@me.com>
2016-01-13 11:36:16 -0600

    got new api working with autoip call and lat+long call

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,22 +2,37 @@ angular.module('weather.controllers', [])
 
 .controller('homeCtrl', function($http) {
   var home = this;
+  var apiKey = 'e267054668bb5a89';
+  var url = 'http://api.wunderground.com/api/' + apiKey + '/';
+  
 
-  home.temp;
-  home.summary;
+  home.temp = "--";
+  home.summary = "Loading...";
   home.icon;
+  home.city;
+  home.state;
+
+
+  $http.get(url+'conditions/forecast/q/autoip.json').then(function (response) {
+    console.log("autoip", response);
+    home.temp = response.data.current_observation.temp_f;
+    home.summary = response.data.current_observation.weather;
+    home.icon = response.data.current_observation.icon_url;
+    home.city = response.data.current_observation.display_location.city;
+    home.state = response.data.current_observation.display_location.state;
+  });
 
   navigator.geolocation.getCurrentPosition(function (geopos) {
-  	var lat = geopos.coords.latitude;
-  	var long = geopos.coords.longitude;
-  	var apiKey = '0a240dea0feab43866d24f9adb42399a';
-  	var url = '/api/forecast/' + apiKey + '/' + lat + ',' + long;
+    var lat = geopos.coords.latitude;
+    var long = geopos.coords.longitude;
 
-  	$http.get(url).then(function (res) {
-  		console.log(res);
-      home.temp = parseInt(res.data.currently.temperature);
-      home.summary = res.data.currently.summary;
-      home.icon = res.data.currently.icon;
+  	$http.get(url+'conditions/forecast/q/'+lat+','+long+'.json').then(function (response) {
+  		console.log("lat+long", response);
+      home.temp = response.data.current_observation.temp_f;
+      home.summary = response.data.current_observation.weather;
+      home.icon = response.data.current_observation.icon_url;
+      home.city = response.data.current_observation.display_location.city;
+      home.state = response.data.current_observation.display_location.state;
   	})
   });
 
